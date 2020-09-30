21ed106e577258651126f0628cb4337895c0da21
victor.phimphachanh <victor.phimphachanh@fdmgroup.com>
2020-09-30 15:44:05 -0400

    Adding todays exercise to the cloud

---
+++ src/main/java/com/fdmgroup/ood2wednesdayexercise/Main.java
@@ -0,0 +1,56 @@
+package com.fdmgroup.ood2wednesdayexercise;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+public class Main {
+	
+	static Scanner sc = new Scanner(System.in);
+	static Logger gLogger = Logger.getLogger("generalLogger");
+	public static void main(String[] args) {
+		User user = new User();
+		prompt(user);
+		writeToFile(user);
+		readFromFile("Users.txt");
+	}
+	private static void readFromFile(String FileName) {
+		try (BufferedReader br = new BufferedReader(new FileReader("Users.txt"))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		}
+		catch (FileNotFoundException e) {
+			gLogger.trace("File Not Found Exception: " + e);
+		} catch (IOException e) {
+			gLogger.trace("IOException could not read line: " + e);
+		}
+	}
+	
+	private static void writeToFile(User user) {
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter("Users.txt",true))) {
+			bw.write(user.getName() + ", " + user.getAddress() + ", " + user.getPhone() + ", " + user.getEmail() + "\n");
+		} catch (IOException e) {
+			gLogger.trace("IOException could not write to file: " + e);
+		}
+	}
+	private static void prompt(User user) {
+		System.out.println("Enter in your name: ");
+		String name = sc.nextLine();
+		System.out.println("Enter in your address: ");
+		String address = sc.nextLine();
+		System.out.println("Enter in your phone: (ex: xxx-xxx-xxxx)");
+		String phone = sc.nextLine();
+		System.out.println("Enter in your email: ");
+		String email = sc.nextLine();
+		user = new User(name,address,phone,email);
+	}
+	
+}
---
+++ src/main/java/com/fdmgroup/ood2wednesdayexercise/User.java
@@ -0,0 +1,54 @@
+package com.fdmgroup.ood2wednesdayexercise;
+
+public class User {
+	
+	private String name;
+	private String address;
+	private String phone;
+	private String email;
+	
+	public User() {
+		
+	}
+	
+	public User(String name, String address, String phone, String email) {
+		super();
+		this.name = name;
+		this.address = address;
+		this.phone = phone;
+		this.email = email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
